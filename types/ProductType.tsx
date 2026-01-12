c478cf01a42d91191fa53f4d95ddf7a1f85275af
phoneCheff <phonecheff@gmail.com>
2026-01-11 22:13:31 -0800

    Fix ui problems and usign cache

--- types/ProductType.tsx
+++ types/ProductType.tsx
@@ -2,15 +2,15 @@
 export type ProductType = {
   id: string;
   name: string;
-  description?: string;
+  description: string;
   price: number;
-  priceWithMargin?: number;
+  priceWithMargin: number;
   coin: string;
-  gender?: string;
-  client_phone?: string;
-  link_images: string[];
+  gender: string;
+  client_phone: string;
+  link_images: { links: string[] };
   custom_slug: string;
-  categories?: Array<{
+  categories: Array<{
     id: string;
     slug: string;
   }>;
